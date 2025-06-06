765c70779e04418ab8ec3ea70871f4e9803047ec
aka <benmo0802@gmail.com>
2025-06-06 14:30:22 +0800

    chore: set images to unoptimized for static export

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,10 @@ const nextConfig: NextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   // Optionally, add any other Next.js config below
-  output: 'export',
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 const withMDX = createMDX({
